fcd7b0ccaa22b98b21b565c7b16e94fd7821432c
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-05-23 11:53:45 -0400

    Added an authentication flow so that users are signed-in anonymously and assigned a unique user id. This prevents one user from seeing another user's data.

---
+++ TodoList/Views/AppEntryView.swift
@@ -0,0 +1,8 @@
+//
+//  AppEntryView.swift
+//  TodoList
+//
+//  Created by Benjamin Lavallee on 2024-05-23.
+//
+
+import Foundation
---
+++ TodoList/Views/AuthView.swift
@@ -0,0 +1,8 @@
+//
+//  AuthView.swift
+//  TodoList
+//
+//  Created by Benjamin Lavallee on 2024-05-23.
+//
+
+import Foundation
