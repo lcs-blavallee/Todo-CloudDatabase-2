ea541f86552c5c2ccce89a7418e4dc341c4de552
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-05-28 09:09:35 -0400

    Used ContentUnavailableView to provide a prompt when no to-do items exist.

--- TodoList/Views/LandingView.swift
+++ TodoList/Views/LandingView.swift
@@ -26,27 +26,35 @@ struct LandingView: View {
             
             VStack {
                 
-                List($viewModel.todos) { $todo in
+                if viewModel.todos.isEmpty {
                     
-                    ItemView(currentItem: $todo)
+                    // Show the prompy to add a new to-do item
+                    ContentUnavailableView(
+                        "No to-do items",
+                        systemImage: "pencil.tip.crop.circle.badge.plus",
+                        description: Text("Add a reminder to get started")
+                    )
+                    
+                } else {
+                    
+                    // Show the list of items
+                    List($viewModel.todos) { $todo in
+                        
+                        ItemView(currentItem: $todo)
                         // Delete item
-                        .swipeActions {
-                            Button(
-                                "Delete",
-                                role: .destructive,
-                                action: {
-                                    viewModel.delete(todo)
-                                }
-                            )
-                        }
-                    
-                }
-                .searchable(text: $searchText)
-                .onChange(of: searchText) {
-                    Task {
-                        try await viewModel.filterTodos(on: searchText)
+                            .swipeActions {
+                                Button(
+                                    "Delete",
+                                    role: .destructive,
+                                    action: {
+                                        viewModel.delete(todo)
+                                    }
+                                )
+                            }
+                        
                     }
                 }
+                
             }
             .navigationTitle("To do")
             // Show the sheet to add a new item
@@ -65,6 +73,13 @@ struct LandingView: View {
                     } label: {
                         Image(systemName: "plus")
                     }
+                }
+            }
+            // Handle searching in the list
+            .searchable(text: $searchText)
+            .onChange(of: searchText) {
+                Task {
+                    try await viewModel.filterTodos(on: searchText)
                 }
             }
         }
